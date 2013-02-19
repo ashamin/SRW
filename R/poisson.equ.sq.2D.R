a68b13f06034b2737a0053d8c6bada5b5535fdae
ashamin <anthony.shamin@gmail.com>
2013-02-19 18:37:16 +0700

    Wrong indices in adressing fixed

--- R/poisson.equ.sq.2D.R
+++ R/poisson.equ.sq.2D.R
@@ -18,7 +18,7 @@
 #' @author Anton Shamin
 
 
-# TODO: divide gi() functions by step
+# TODO: implement case when I != J
 
 init <- function(){
   source("R/precond.R")
@@ -49,14 +49,14 @@ right.answer <- function(x, y) x + 3*x^2 - 10*x*y - 2*y - 4*y^2 + (p-15)*sin(pi*
 
 slv.poisson <- function(a=a.test, b=b.test, g1=g1.test, g2=g2.test,
                 g3=g3.test, g4=g4.test, f=f.test,
-                I=10, J=10, h1=a/(I-1), h2=b/(J-1)){
+                I=20, J=20, h1=a/(I-1), h2=b/(J-1)){
   
   x <- seq(from=0, to=a, by=h1)
   y <- seq(from=0, to=b, by=h2)
   
   n <- (I-2)*(J-2)
   
-  A <- form.A.matrix(n, h1, h2, I)
+  A <- form.A.matrix(n, h1, h2, I, J)
     
   b <- form.b(n, I, J, x, y, h1, h2, f, g1, g2, g3, g4)
   
@@ -66,20 +66,31 @@ slv.poisson <- function(a=a.test, b=b.test, g1=g1.test, g2=g2.test,
 #       ra[i, j] = right.answer(x[i], y[j])
 #   image(x, y, ra)
   
-  res = data.frame(solve = slv(A, b, P=SSOR.precond.approach1(A, .8))$root, 
+  res = data.frame(solve = slv(A, b, P=SSOR.precond.approach1(A, .8), maxit=1000)$root, 
                   answer = get.res.vector(n, I, J, x, y))
   
   View(res)
+  
+  print(h1)
+  print(h2)
+  print(as.double(A[1,]%*%res$answer))
+  print(b[1])
+  View(A)
+  View(b)
 }
 
-form.A.matrix <- function(n, h1, h2, I){
+form.A.matrix <- function(n, h1, h2, I, J){
   A <- matrix(rep(0, times=n^2), n, n)
   
   d <- diag(A)
   diag.num <- -outer(seq(d),seq(d),"-")
   A[diag.num == 0] = rep(-(2/h1^2 + 2/h2^2), times=length(A[diag.num == 0]))
   A[diag.num == 1] = A[diag.num == -1] = rep(1/h1^2, times=length(A[diag.num == 1]))
-  A[diag.num == 1+(I-2)] = A[diag.num == -1-(I-2)] = rep(1/h2^2, times=length(A[diag.num == 1+(I-2)]))
+  A[diag.num == (I-2)] = A[diag.num == -(J-2)] = rep(1/h2^2, times=length(A[diag.num == (I-2)]))
+  
+  d <- A[diag.num == 1]
+  d[seq(from=(I-2), to=length(d), by=(I-2))] = 0
+  A[diag.num == 1] = A[diag.num == -1] = d
   
   return (A)
 }
@@ -91,10 +102,10 @@ form.b <- function(n, I, J, x, y, h1, h2, f, g1, g2, g3, g4){
     for (i in 2:(I-1)){
       k = (I-2)*(j-2) + (i-2) + 1
       b[k] = -f(x[i], y[j])
-      if (j == 2) b[k] = b[k] - g3(x[i])
-      if (j == (J-1)) b[k] = b[k] - g4(x[i])
-      if (i == 2) b[k] = b[k] - g1(y[i])
-      if (i == (I-1)) b[k] = b[k] - g2(y[i])
+      if (j == 2) b[k] = b[k] - g3(x[i])/h2^2
+      if (j == (J-1)) b[k] = b[k] - g4(x[i])/h2^2
+      if (i == 2) b[k] = b[k] - g1(y[j])/h1^2
+      if (i == (I-1)) b[k] = b[k] - g2(y[j])/h1^2
     }
   
   return (b)
