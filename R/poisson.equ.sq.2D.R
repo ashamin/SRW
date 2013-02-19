60acaca6cd4171fe8c203361b23b2c36d67fcedf
ashamin <anthony.shamin@gmail.com>
2013-02-19 22:24:56 +0700

    Method monitoring added
    1) Drawing temperaturegramms
    2) Residual norm and number of iterations computing
    3) Few tests

--- R/poisson.equ.sq.2D.R
+++ R/poisson.equ.sq.2D.R
@@ -18,7 +18,9 @@
 #' @author Anton Shamin
 
 
-# TODO: implement case when I != J
+# TODO: 
+#   1) implement case when I != J
+#   2) include borders to temperaturegrams
 
 init <- function(){
   source("R/precond.R")
@@ -41,6 +43,66 @@ g4.test <- function(x) 3*x^2 - 9*x - 6
 f.test <- function(x, y)  2 + 2*pi^2*(p - 15)*sin(pi*x)*sin(pi*y)
 
 right.answer <- function(x, y) x + 3*x^2 - 10*x*y - 2*y - 4*y^2 + (p-15)*sin(pi*x)*sin(pi*y)
+
+
+#Test 2
+# p <- 3
+# a.test <- 1
+# b.test <- pi/2
+# 
+# g1.test <- function(y) sin(p*y)
+# g2.test <- function(y) exp(p)*sin(p*y)
+# g3.test <- function(x) 0
+# g4.test <- function(x) exp(p*x)*sin(p*pi/2)
+# 
+# f.test <- function(x, y)  0
+# 
+# right.answer <- function(x, y) exp(p*x)*sin(p*y)
+
+
+#Test 3
+# p <- 8
+# a.test <- 2
+# b.test <- 1
+# 
+# g1.test <- function(y) 0
+# g2.test <- function(y) p*y/4 + 4*cos(y)
+# g3.test <- function(x) 2*x
+# g4.test <- function(x) p*x/8 + 2*x*cos(1)
+# 
+# f.test <- function(x, y)  2*x*cos(y)
+# 
+# right.answer <- function(x, y) p*x*y/8 + 2*x*cos(y)
+
+
+#Test 4
+# p <- 12
+# a.test <- 1
+# b.test <- 1
+# 
+# g1.test <- function(y) -sqrt(p)*y^2
+# g2.test <- function(y) (sqrt(p) + y)*(1 - y^2)
+# g3.test <- function(x) sqrt(p)*x^2
+# g4.test <- function(x) (sqrt(p) + x)*(x^2 - 1)
+# 
+# f.test <- function(x, y)  0
+# 
+# right.answer <- function(x, y) (sqrt(p) + x*y)*(x^2 - y^2)
+
+
+#Test 5
+# p <- 23
+# a.test <- 1
+# b.test <- 1
+# 
+# g1.test <- function(y) exp(-y^2)
+# g2.test <- function(y) y/p + exp(1-y^2)
+# g3.test <- function(x) exp(x^2)
+# g4.test <- function(x) x/p + exp(x^2 - 1)
+# 
+# f.test <- function(x, y)  -4*x^2*exp(x^2 - y^2) - 4*y^2*exp(x^2 - y^2)
+# 
+# right.answer <- function(x, y) x*y/p + exp(x^2 - y^2)
 #
 #
 #
@@ -49,7 +111,7 @@ right.answer <- function(x, y) x + 3*x^2 - 10*x*y - 2*y - 4*y^2 + (p-15)*sin(pi*
 
 slv.poisson <- function(a=a.test, b=b.test, g1=g1.test, g2=g2.test,
                 g3=g3.test, g4=g4.test, f=f.test,
-                I=20, J=20, h1=a/(I-1), h2=b/(J-1)){
+                I=10, J=10, h1=a/(I-1), h2=b/(J-1)){
   
   x <- seq(from=0, to=a, by=h1)
   y <- seq(from=0, to=b, by=h2)
@@ -60,23 +122,13 @@ slv.poisson <- function(a=a.test, b=b.test, g1=g1.test, g2=g2.test,
     
   b <- form.b(n, I, J, x, y, h1, h2, f, g1, g2, g3, g4)
   
-#   ra <- matrix(rep(0, length(x)*length(y)), length(x), length(y), byrow=TRUE)
-#   for (i in 1:length(x))
-#     for (j in 1:length(y))
-#       ra[i, j] = right.answer(x[i], y[j])
-#   image(x, y, ra)
+  print.temperaturegrams(x, y, A, b, I, J)
   
-  res = data.frame(solve = slv(A, b, P=SSOR.precond.approach1(A, .8), maxit=1000)$root, 
-                  answer = get.res.vector(n, I, J, x, y))
-  
-  View(res)
-  
-  print(h1)
-  print(h2)
-  print(as.double(A[1,]%*%res$answer))
-  print(b[1])
-  View(A)
-  View(b)
+#   res = data.frame(solve = slv(A, b, P=SSOR.precond.approach1(A, .8), maxit=1000)$root, 
+#                   answer = get.res.vector(n, I, J, x, y))
+#   
+#   View(res)
+
 }
 
 form.A.matrix <- function(n, h1, h2, I, J){
@@ -109,6 +161,22 @@ form.b <- function(n, I, J, x, y, h1, h2, f, g1, g2, g3, g4){
     }
   
   return (b)
+}
+
+print.temperaturegrams <- function(x, y, A, b, I, J){
+  ra <- matrix(rep(0, (length(x)-2)*(length(y)-2)), length(x)-2, length(y)-2, byrow=TRUE)
+  for (i in 2:(length(x)-1))
+    for (j in 2:(length(y)-1))
+      ra[i-1, j-1] = right.answer(x[i], y[j])
+  par(mfcol=c(1, 2))
+  
+  s <- slv(A, b, P=SSOR.precond.approach1(A, 1), maxit=1000)
+  
+  sl <- matrix(s$root, (I-2), (J-2), byrow=FALSE)
+  image(x[2:(I-1)], y[2:(J-1)], sl)
+  image(x[2:(I-1)], y[2:(J-1)], ra)
+  
+  print(data.frame(residual.norm=norm(ra - sl)/norm(sl), iteration.number=unique(s$it)))  
 }
 
 get.res.vector <- function(n, I, J, x, y){
