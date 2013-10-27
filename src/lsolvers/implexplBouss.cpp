6733aa4bcae3080d609564a4262462aac38e8b70
ashamin <anthony.shamin@gmail.com>
2013-10-27 16:58:52 +0400

    basics of differential schemes done for #9 #10

--- src/lsolvers/implexplBouss.cpp
+++ src/lsolvers/implexplBouss.cpp
@@ -27,6 +27,25 @@ implexplBouss::~implexplBouss(){
 
 void implexplBouss::formDiffOperators(){
     // here we form Tx, Ty and mu vaues on each step (parallel)
+    // here we also should insert zeros to lower and upper diags
+
+    // what to do with H values on borders - add new array
+
+    dx_l[0] = dy_l[0] = dx_u[n-1] = dy_u[n-1] = 0;
+
+    for (int i = 1; i<n-1; i++){
+        dx_l[i] = Tx((H[i-1] + H[i])/2);
+        dx_u[i] = Tx((H[i+1] + H[i])/2);
+        dx_d[i] = dx_l[i]+dx_u[i];
+    }
+
+    for (int i = 1; i<n-1; i++){
+        dy_l[i] = Ty((H[i-1] + H[i])/2);
+        dy_u[i] = Ty((H[i+1] + H[i])/2);
+        dy_d[i] = dy_l[i]+dy_u[i];
+    }
+
+    // here must fix border values of H
 }
 
 double* implexplBouss::solve(){
